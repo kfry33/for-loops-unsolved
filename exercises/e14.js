1f4e2374ee4cbd1301796464f181602cbbd020ce
Kasandra Fry <k.fry@phoenixenv.com>
2023-05-18 09:03:27 -0400

    14th solved

--- exercises/e14.js
+++ exercises/e14.js
@@ -7,10 +7,28 @@
 
 export function getClientsWithWrongBalance(array) {
   // Your code goes here...
-
-}
-
-
+  let wrongBal = [];
+  let sum = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    if (!array[i].withdrawals) {
+      sum = 0;
+    } else {
+      for (let amount of array[i].withdrawals) {
+      sum -= amount
+      }
+    } 
+    if (!array[i].deposits) {
+      sum = 0
+    } else {
+      for (let amount of array[i].deposits) {
+        sum += amount
+      }
+    } if (sum != array[i].balance) {
+      wrongBal.push(array[i])
+    } 
+  } return wrongBal;  
+} 
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-14"
