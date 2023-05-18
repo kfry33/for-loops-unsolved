4088c38cfa79bb676174b0f4a7f84bb3b625dcd2
Kasandra Fry <k.fry@phoenixenv.com>
2023-05-18 09:45:12 -0400

    15th solved

--- exercises/e15.js
+++ exercises/e15.js
@@ -6,7 +6,13 @@
 
 export function concatArrays(arr1, arr2) {
   // Your code goes here...
-
+  let fullArray = [];
+  for (let i = 0; i < arr1.length; i++) {
+    fullArray.push(arr1[i])
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    fullArray.push(arr2[i])
+  } return fullArray;
 }
 
 
