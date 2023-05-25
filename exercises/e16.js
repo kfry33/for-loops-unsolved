7970278f538d874f35c9874f36ae54281ffd954d
Kasandra Fry <k.fry@phoenixenv.com>
2023-05-25 15:23:18 -0400

    eliminated includes

--- exercises/e16.js
+++ exercises/e16.js
@@ -10,7 +10,9 @@ export function flatArrays(array) {
   let flattened = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      flattened.push(...array[i]);
+      for (let j=0; j<array[i].length; j++) {
+        flattened.push(array[i][j]);
+      }
     } else {
       flattened.push(array[i]);
     }
